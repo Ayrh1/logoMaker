2fa6cb89d60c5c8b5e022d50192ae1c27e543559
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-10-25 14:50:31 -0700

    writeToFile

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
-const  genLogo = require("../genLogo.js");
+const  genLogo = require("./lib/genLogo.js");
 
 inquirer.prompt([
     {
         type: "input",
-        name: "name",
-        message: "what ius your name?"
+        name: "text",
+        message: "Please Enter Three letters:",
+        validate: function (input) {
+            if (input.length > 3) {
+            return 'Please enter a valid put, 3 letters max.';
+            }
+            return true;
+        }
     },
     {
         type: "input",
-        name: "location",
-        message: "where are your from?"
+        name: "textColor",
+        message: "Please enter text color,ex #00000:"
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: "Please choose shape:",
+        choices: ["Circle", "Triangle", "Rectangle", "Random"],
     },
     {
         type: "input",
-        name: "github",
-        message: "what is your Github URL?"
-    },
-    {
-        type: "input",
-        name: "linkedin",
-        message: "what is your linked in URL"
+        name: "shapeColor",
+        message: "Please enter shape color, ex #00000:"
     },
 
 ]).then((data) => {
-
-    const generatedHtmlCode = generateHTML(data)
-
-    fs.writeFile('index.html', generatedHtmlCode, (error) => {
-    error ? console.log(error) : console.log("successful created index.html")
+    const generatedSvgCode = genLogo(data)
+    const filePath = `./examples/${data.text}.svg`;
+    fs.writeFile(filePath, generatedSvgCode, (error) => {
+    error ? console.log(error) : console.log(`successful created ${data.text}.svg`)
     })
 
 })
